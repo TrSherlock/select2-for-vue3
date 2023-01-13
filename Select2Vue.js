adc407a0f5e37061b26c0359c905c8dd41cda29e
TrSherlock <33941709+TrSherlock@users.noreply.github.com>
2023-01-14 00:44:43 +0300

    Update Select2Vue.js

--- Select2Vue.js
+++ Select2Vue.js
@@ -3,7 +3,7 @@ const Select2Vue = {
     <div class="selection" @click="openSelect">
         <div class="select2-selection select2-selection--single">
         <input style="display:none;" class="select2-selection__rendered" type="button" ref="formInput" v-model="modelValue" :name="formname">
-        <span className="select2-selection__rendered" :title="modelValue">{{modelValue}}</span>
+        <span className="select2-selection__rendered" :title="modelValue">{{text.map(a=>options[setIndex][a]).filter(b=>b!==null).join("; ")}}</span>
             <span class="select2-selection__arrow"><b></b></span>
         </div></div>
     <div class="select2-container select2-container--default select-container--open select2-container-dropdown">
@@ -12,7 +12,6 @@ const Select2Vue = {
     <input
     class="select2-search__field"
     ref="findText"
-    @change="change($event)"
     v-model="searchText"
     @keyup="find($event, 'up')"
     @keydown="find($event, 'down')">
@@ -23,15 +22,14 @@ const Select2Vue = {
     class="ow select2-results__option hover:bg-violet-300"
     :class="{ 'select2-results__option--highlighted': index===setIndex }"
     v-for="(item, index) in filters"
-    :data-setIndex="setIndex"
     :data-index="index"
-    :data-value="item[id]"
+    :data-value="item.option[id]"
     :data-selected="index===setIndex"
-    @click="select($event, index, 'click')"
-    >{{text.map(a=>item[a]).filter(b=>b!==null).join("; ")}}</li>
+    @click="select($event, item.index, 'click')"
+    >{{text.map(a=>item.option[a]).filter(b=>b!==null).join("; ")}}</li>
     </ul></div></div></div></div>`,
     props: {
-        modelValue: String,
+        modelValue: [String, Number],
         options: {
             type: Array
         },
@@ -52,7 +50,6 @@ const Select2Vue = {
         }
     },
     mounted() {
-        this.setIndex = this.options.findIndex(i => i[this.id] === this.modelValue);
         this.setFilter(this.options);
         const handleClick = (event) => {
             if (this.$refs.selectRef && !this.$refs.selectRef.contains(event.target)) {
@@ -62,48 +59,60 @@ const Select2Vue = {
         document.addEventListener("click", handleClick);
     },
     watch: {
-        options(newValue) {
-            this.setFilter(newValue);
-        },
-        modelValue(){
-            this.setIndex = this.filters.findIndex(i => i[this.id] === this.modelValue);
-            this.scrollHeightCenter(this.$refs.ul.children[this.setIndex]);
+        modelValue() {
+
+            this.setIndex = this.filters.findIndex(i => i.option[this.id] === this.modelValue);
         },
         searchText(newtext) {
             if (newtext !== "") {
-                this.filters = this.options.filter(f => {
+                this.filters = this.options.map((f, i) => {
                     const searchingText = this.text.map(a => f[a]).filter(b => b !== null).join("; ");
-                    return searchingText.toLocaleUpperCase().includes(newtext.toLocaleUpperCase());
-                })
-                this.setIndex = this.filters.findIndex(i => i[this.id] === this.modelValue);
+                    if (searchingText.toLocaleUpperCase().includes(newtext.toLocaleUpperCase())) {
+                        return {
+                            index: i,
+                            option: f
+                        };
+                    };
+                }).filter(f => f !== undefined);
+
+                this.setIndex = this.filters.findIndex(i => i.option[this.id] === this.modelValue);
                 if (this.setIndex == -1) {
                     this.setIndex = 0;
                 }
+                this.scrollHeightCenter();
+
             } else {
-                this.filters = this.options;
-                this.setIndex = this.options.findIndex(i => i[this.id] === this.modelValue);
+                this.setFilter(this.options);
+                this.scrollHeightCenter();
             }
         }
     },
     methods: {
-        setFilter(newValue) {
-            this.filters = newValue;
-        },
-        change(e) {
-            console.log(e.target.value)
+        async setFilter(newValue) {
+            this.filters =
+                newValue.map((e, i) => {
+                    return {
+                        index: i,
+                        option: e
+                    }
+                });
+            this.setIndex = this.filters.findIndex(i => i.option[this.id] === this.modelValue);
         },
         openSelect(event) {
-            // this.filters = this.options
             event.target.closest(".select2").classList.toggle("select2-container--open")
+            this.searchText = "";
             this.$refs.findText.focus();
-            this.setIndex = this.filters.findIndex(i => i[this.id] === this.modelValue);
-            this.scrollHeightCenter(this.$refs.ul.children[this.setIndex]);
+            this.scrollHeightCenter();
         },
         find(e, updown) {
             const lastIndex = this.filters.lastIndexOf(this.filters.at(-1));
+
             if (updown == "down") {
                 switch (e.keyCode) {
                     case 107: console.log(e.target.value + 1e3);
+                        break;
+                    case 110:
+                        e.preventDefault();
                         break;
                     case 38:
                         if (updown == "down") {
@@ -121,37 +130,49 @@ const Select2Vue = {
             } else {
                 switch (e.keyCode) {
                     case 13:
-                        this.$emit('update:modelValue', this.filters[this.setIndex][this.id]);
+                        const filterIndex = this.filters[this.setIndex].index;
+                        this.$emit('update:modelValue', this.options[filterIndex][this.id]);
+                        this.$emit('change', this.options[filterIndex][this.id]);
                         this.$refs.selectRef.classList.remove("select2-container--open");
+
+                        this.setFilter(this.options);
+                        break;
+                    case 110:
+                        e.target.value += ".";
                         break;
                     case 27:
                         this.$refs.selectRef.classList.remove("select2-container--open");
                     case 38:
                         e.preventDefault();
                         this.setIndex = (this.setIndex === 0) ? lastIndex : this.setIndex - 1;
-                        this.scrollHeightCenter(this.$refs.ul.children[this.setIndex]);
+                        this.scrollHeightCenter();
                         break;
                     case 40:
                         e.preventDefault();
                         this.setIndex = (this.setIndex === lastIndex) ? 0 : this.setIndex + 1;
-                        this.scrollHeightCenter(this.$refs.ul.children[this.setIndex]);
+                        this.scrollHeightCenter();
                         break;
                     default:
                         break;
                 }
             }
         },
-        scrollHeightCenter($ref) {
-            const $refParent = $ref.parentElement;
-            $refParent.scrollTop = $ref.offsetTop - $refParent.offsetTop - ($refParent.offsetHeight / 2 - $ref.offsetHeight / 2);
+        scrollHeightCenter() {
+            this.$nextTick(() => {
+                const $ref = this.$refs.ul.children[this.setIndex]
+                const $refParent = $ref.parentElement;
+                $refParent.scrollTop = $ref.offsetTop - $refParent.offsetTop - ($refParent.offsetHeight / 2 - $ref.offsetHeight / 2);
+            })
         },
         select(event, i, eventType) {
             this.setIndex = i
-            this.$emit('update:modelValue', this.filters[i][this.id]);
+            this.$emit('update:modelValue', this.options[i][this.id]);
+            this.$emit('change', this.options[i][this.id]);
             this.selectElement = event.target;
             this.$refs.ul.scrollTop = event.target.offsetTop - this.$refs.ul.offsetTop - (this.$refs.ul.offsetHeight / 2 - event.target.offsetHeight / 2);
             this.$refs.selectRef.classList.remove("select2-container--open");
+            this.setFilter(this.options);
         }
     },
-    emits: ['update:modelValue']
+    emits: ['change', 'update:modelValue']
 }
